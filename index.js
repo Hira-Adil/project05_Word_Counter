7e8fc550d6d7e4e1581ae47355e11f4723a1dc81
Hira-Adil <adilkhan2002143@gmail.com>
2024-05-06 14:21:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Print welcome message
+console.log(chalk.green("\n \t Welcome To Word Counter App\n"));
+let anser = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Enter Your Sentence"
+});
+let wordCount = anser.words.trim().split(" ").length;
+console.log(`Your Sentence has ${wordCount} words.`);
+console.log(chalk.green("\nThanks For Using Word Counter App\n"));
